685e4cd39b483866364df723f5dde8da7b4680cf
Bas Nijholt <bas@nijho.lt>
2020-09-26 13:12:06 +0200

    rephase some text

--- custom_components/adaptive_lighting/__init__.py
+++ custom_components/adaptive_lighting/__init__.py
@@ -1,30 +1,30 @@
 """
 Adaptive Lighting Component for Home-Assistant.
 
-This component calculates color temperature and brightness to synchronize
-your color changing lights with perceived color temperature of the sky throughout
-the day. This gives your environment a more natural feel, with cooler whites during
-the midday and warmer tints near twilight and dawn.
 
-In addition, the component sets your lights to a nice warm white at 1% in "Sleep" mode,
-which is far brighter than starlight but won't reset your adaptive rhythm or break down
-too much rhodopsin in your eyes.
+This component calculates color temperature and brightness to synchronize
+your color-changing lights with the perceived color temperature of the sky
+throughout the day. This gives your environment a more natural feel, with
+cooler whites during the midday and warmer tints near twilight and dawn.
+
+Additionally, the component sets your lights to a nice warm white at 1% in
+"Sleep mode", which is far brighter than starlight but won't reset your
+circadian rhythm or break down too much rhodopsin in your eyes.
 
 Human circadian rhythms are heavily influenced by ambient light levels and
-hues. Hormone production, brainwave activity, mood and wakefulness are
+hues. Hormone production, brainwave activity, mood, and wakefulness are
 just some of the cognitive functions tied to cyclical natural light.
-http://en.wikipedia.org/wiki/Zeitgeber
 
-Further reading:
-
+Resources:
+- http://en.wikipedia.org/wiki/Zeitgeber
 - http://www.cambridgeincolour.com/tutorials/sunrise-sunset-calculator.htm
 - http://en.wikipedia.org/wiki/Color_temperature
 
-Technical notes: I had to make a lot of assumptions when writing this app
-*   There are no considerations for weather or altitude, but does use your
-    hub's location to calculate the sun position.
-*   The component doesn't calculate a true "Blue Hour" -- it just sets the
-    lights to 2700K (warm white) until your hub goes into Night mode
+## Notes
+* Only your location is taken into account to calculate the the sun's position.
+* Weather and altitude are not considered.
+* The component does not calculate a true "Blue Hour" -- it just sets the
+  lights to 2700K (warm white) until your hub goes into "Sleep mode".
 """
 import asyncio
 import logging
