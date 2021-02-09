e4aa570f176450474e83aa95d9f9f59ad2a4a07f
igiannakas <59056762+igiannakas@users.noreply.github.com>
2021-02-09 12:41:16 +0000

    Update switch.py
    Continue to adapt color temperature down to the sleep temperature after sunset. Results in a gradually warming light during the night time rather than a fixed color temperature throughout the night time.

--- custom_components/adaptive_lighting/switch.py
+++ custom_components/adaptive_lighting/switch.py
@@ -1098,6 +1098,9 @@ class SunLightSettings:
         if percent > 0:
             delta = self.max_color_temp - self.min_color_temp
             return (delta * percent) + self.min_color_temp
+        if percent < 0:
+            delta = abs(self.min_color_temp - self.sleep_color_temp)
+            return (delta * abs(1+percent)) + self.sleep_color_temp
         return self.min_color_temp
 
     def get_settings(
