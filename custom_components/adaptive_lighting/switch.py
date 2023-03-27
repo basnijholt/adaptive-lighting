0f50d9a3fbbd1e7e140b5721f6b36fcd6d4aa622
Benjamin Auquite <halomastar@gmail.com>
2023-03-27 02:01:19 -0500

    Update switch.py

--- custom_components/adaptive_lighting/switch.py
+++ custom_components/adaptive_lighting/switch.py
@@ -558,7 +558,10 @@ def _expand_light_groups(hass: HomeAssistant, lights: list[str]) -> list[str]:
 
 def _supported_features(hass: HomeAssistant, light: str):
     state = hass.states.get(light)
-    supported_features = state.attributes.get(ATTR_SUPPORTED_FEATURES, 0)
+    if not state:
+        supported_features = 0
+    else:
+        supported_features = state.attributes.get(ATTR_SUPPORTED_FEATURES, 0)
     supported = {
         key for key, value in _SUPPORT_OPTS.items() if supported_features & value
     }
