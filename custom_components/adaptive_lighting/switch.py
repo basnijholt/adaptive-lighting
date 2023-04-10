0afffd90e5dee20e0e6a274b1d5d7ab0bee10a4f
Benjamin Auquite <halomastar@gmail.com>
2023-04-10 17:46:22 -0500

    Update switch.py

--- custom_components/adaptive_lighting/switch.py
+++ custom_components/adaptive_lighting/switch.py
@@ -1326,7 +1326,7 @@ class AdaptiveSwitch(SwitchEntity, RestoreEntity):
             if self._dim_to_warm and "brightness" in features:
                 cur_state = None
                 if self._dim_to_warm_brightness_check:
-                    await self.hass.helpers.entity_component.update_entity(light)
+                    self.hass.helpers.entity_component.update_entity(light)
                     cur_state = self.hass.states.get(light)
                 if cur_state:
                     brightness = cur_state.attributes[ATTR_BRIGHTNESS]
