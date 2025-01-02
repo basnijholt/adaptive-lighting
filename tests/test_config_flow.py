basnijholt/adaptive-lighting

commit bf7d109c9833c4404d212c3bab38b54ec7a25021

fix: async entry setup (#1141)
* Update __init__.py

* Update strings.json

custom_components/adaptive_lighting/__init__.py
@@ -65,10 +65,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
 
     undo_listener = config_entry.add_update_listener(async_update_options)
     data[config_entry.entry_id] = {UNDO_UPDATE_LISTENER: undo_listener}
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(config_entry, platform),
-        )
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
     return True
 

commit b1e3e629cd0349969c4e4e4d997af89b5e90563e

Fix `test_changing_options_when_using_yaml` (broke after #1141)

tests/test_config_flow.py
@@ -1,5 +1,9 @@
 """Test Adaptive Lighting config flow."""
 
+from collections.abc import Generator
+from unittest.mock import AsyncMock, patch
+
+import pytest
 from homeassistant.components.adaptive_lighting.const import (
     CONF_SUNRISE_TIME,
     CONF_SUNSET_TIME,
@@ -114,10 +118,24 @@ async def test_import_twice(hass):
         )
 
 
+@pytest.fixture
+def mock_config_entries_async_forward_entry_setup() -> Generator[AsyncMock]:
+    """Mock async_forward_entry_setup."""
+    with patch(
+        "homeassistant.config_entries.ConfigEntries.async_forward_entry_setups",
+    ) as mock_fn:
+        yield mock_fn
+
+
 # TODO: Fix, broken for all supported versions
 # But in ≤2024.5 it gives homeassistant.config_entries.UnknownEntry: cd69dbda65bd3f86e9a32d974cdfa23f
 # and ≥2024.6 it times out
-async def test_changing_options_when_using_yaml(hass):
+
+
+async def test_changing_options_when_using_yaml(
+    hass,
+    mock_config_entries_async_forward_entry_setup,
+):
     """Test changing options when using YAML."""
     entry = MockConfigEntry(
         domain=DOMAIN,